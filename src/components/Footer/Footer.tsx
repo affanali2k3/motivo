e2d1392cafdde8ed4c454166d7bba57ff9d3ade2
aliTurab1122911 <aliturabulhussnain1122911@gmail.com>
2024-02-21 03:28:53 +0500

    updated entire site for desktop view

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,9 @@
 import { PrimaryButton } from "../PrimaryButton/PrimaryButton";
 import "./Footer.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnet } from '@fortawesome/free-solid-svg-icons';
+
+
 export const Footer = () => {
   return (
     <footer className="footer-main-div">
@@ -9,9 +13,9 @@ export const Footer = () => {
         </h1>
         <div className="footer-points-cta-div">
           <ul className="footer-points">
-            <li>Get live walkthrough of how we work</li>
-            <li>Explore use cases for your team</li>
-            <li>Explore best pricing plan for you</li>
+            <li><FontAwesomeIcon id="fontIconMag" icon={faMagnet} rotation={90} style={{color: "#1b35c7",}} />  Get live walkthrough of how we work</li>
+            <li><FontAwesomeIcon id="fontIconMag" icon={faMagnet} rotation={90} style={{color: "#1b35c7",}} />  Explore use cases for your team</li>
+            <li><FontAwesomeIcon id="fontIconMag" icon={faMagnet} rotation={90} style={{color: "#1b35c7",}} />  Explore best pricing plan for you</li>
             <p className="contact-support">
               looking for support? connect with our <span>Support Team</span>
             </p>
