d756fc3744ba21e63bffe20277b6ed6d95b996bc
affanali2k3 <affanalikhanxx@gmail.com>
2024-02-26 00:36:43 +0500

    [NEW MODELS + MODEL RESPONSIVENESS + MEET TEAM PICTURE CHANGED]

--- src/components/MeetOurTeamHuggingModel/MeetOurTeamHuggingModel.tsx
+++ src/components/MeetOurTeamHuggingModel/MeetOurTeamHuggingModel.tsx
@@ -1,9 +1,9 @@
-// import Spline from "@splinetool/react-spline";
+import Spline from "@splinetool/react-spline";
 import "./MeetOurTeamHuggingModel.scss";
 export const MeetOurTeamHuggingModel = () => {
   return (
     <section className="meet-our-team-hugging-model-main-div">
-      {/* <Spline scene="https://prod.spline.design/eD5ufWGutIT3wc0W/scene.splinecode" /> */}
+      <Spline scene="https://prod.spline.design/UsVfZ79WvpMHALl1/scene.splinecode" />
     </section>
   );
 };
--- src/components/ServicesDisplayPage/ServicesDisplayPage.tsx
+++ src/components/ServicesDisplayPage/ServicesDisplayPage.tsx
@@ -6,8 +6,8 @@ export const ServicesDisplayPage = () => {
   return (
     <section className="services-display-page-main-div">
       <div className="services-display-corner-rounded-div">
+        <ServicesPageThumbsUpModel />
         <div className="digital-marketing-service-card first-service">
-          <ServicesPageThumbsUpModel />
           <div className="content">
             <div className="header">
               <h3>Digital marketing & advertising services</h3>
@@ -23,12 +23,9 @@ export const ServicesDisplayPage = () => {
                 )}
               </div>
               <div className="para">
-                Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                vulputate libero et velit interdum, ac aliquet odio mattis.Corem
-                ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                vulputate libero et velit interdum, ac aliquet odio mattis.ac
-                aliquet odio mattis.Nunc vulputate libero et velit interdum, ac
-                aliquet odio mattis.ac aliquet odio mattis.
+                Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio
+                mattis.Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio
+                mattis.ac aliquet odio mattis.Nunc vulputate libero et velit interdum, ac aliquet odio mattis.ac aliquet odio mattis.
               </div>
             </div>
           </div>
@@ -49,12 +46,9 @@ export const ServicesDisplayPage = () => {
                 )}
               </div>
               <div className="para">
-                Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                vulputate libero et velit interdum, ac aliquet odio mattis.Corem
-                ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                vulputate libero et velit interdum, ac aliquet odio mattis.ac
-                aliquet odio mattis.Nunc vulputate libero et velit interdum, ac
-                aliquet odio mattis.ac aliquet odio mattis.
+                Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio
+                mattis.Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio
+                mattis.ac aliquet odio mattis.Nunc vulputate libero et velit interdum, ac aliquet odio mattis.ac aliquet odio mattis.
               </div>
             </div>
           </div>
